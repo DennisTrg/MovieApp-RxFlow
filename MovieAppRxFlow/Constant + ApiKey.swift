98c3b8bd0330c3ebd200454c5a1636c4bef9862d
Tung Truong <tungtruongthe@gmail.com>
2023-01-10 20:50:32 +0700

    [ADD] add constant file + api key

---
+++ MovieAppRxFlow/Constant + ApiKey.swift
@@ -0,0 +1,32 @@
+//
+//  Constant + ApiKey.swift
+//  MovieAppRxFlow
+//
+//  Created by Tung Truong on 10/01/2023.
+//
+
+import Foundation
+
+let APIKey = "5f5fa0af154df4ddc2d383fc80896ed7"
+
+let genreCode = [
+    28 : "Action",
+    12 : "Adventure",
+    16 : "Animation",
+    35 : "Comedy",
+    80 : "Crime",
+    99 : "Documentary",
+    18 : "Drama",
+    10751 : "Family",
+    14 : "Fantasy",
+    36 : "History",
+    27 : "Horror",
+    10402 : "Music",
+    9648 : "Mystery",
+    10749 : "Romance",
+    878 : "Science Fiction",
+    10770 : "TV Movie",
+    53 : "Thriller",
+    10752 : "War",
+    37 : "Western"
+]
